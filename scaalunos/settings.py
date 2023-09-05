70d5b153552755989b708b0464178254b11c1ef9
victor <victorvidigal@gmail.com>
2023-09-05 20:09:34 -0300

    Alterar bd para postgresql

--- scaalunos/settings.py
+++ scaalunos/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'scaalunos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sca',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
