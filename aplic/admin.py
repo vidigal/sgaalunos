c809bc110a667edfc16219ea7fc8770abfc67300
Victor Vidigal Ribeiro <victor.ribeiro1@granbery.edu.dti>
2023-09-06 21:50:43 -0300

    Cadastro de cursos

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from aplic.models import Curso
+
 # Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria')
--- aplic/models.py
+++ aplic/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Curso(models.Model):
+    nome = models.CharField(_('Nome'), max_length=100)
+    descricao = models.TextField(_('Descrição'), max_length=500)
+    carga_horaria = models.IntegerField(_('Carga Horária'))
+
+    class Meta:
+        verbose_name = _('Curso')
+        verbose_name_plural = _('Cursos')
+
+    def __str__(self):
+        return self.nome
